0d951764a5f30b91803a28fde20e3ddf62fd8461
shainadeguzman <shainajanedg@gmail.com>
2025-04-29 23:12:51 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import NotificationBar from '@/components/NotificationBar.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Header.vue'
 </script>
 
 <template>
+    <NotificationBar />
+    <Header />
+    <Footer />
 </template>
 
 <style scoped>
