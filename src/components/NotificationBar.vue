f518bda3bb71234756ba425bbd8ba5182e988dcb
shainadeguzman <shainajanedg@gmail.com>
2025-04-30 16:00:40 +0800

    fix

--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -1,5 +1,8 @@
 <template>
-    test
+    <div>
+        <span>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</span>
+        <span>ShopNow</span>
+    </div>
 </template>
 
 <script setup></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <template>
-    home
 </template>
 
 <script setup></script>
