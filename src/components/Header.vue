db7b011a0918bec762c7f1f48f47d6bc1bbafa90
shainadeguzman <shainajanedg@gmail.com>
2025-04-30 11:20:41 +0800

    add router and route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,10 @@
     </div>
 </template>
 
-<script></script>
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter() // Use this to access the router instance
+const route = useRoute() // Use this to access the current route
+</script>
 
 <style scoped></style>
