53d0ebfbd5f0fd47f19b5ce3969105e2e9ff44d2
shainadeguzman <shainajanedg@gmail.com>
2025-04-30 16:23:55 +0800

    update notification bar

--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <span>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</span>
-        <span>ShopNow</span>
+    <div class="bg-black text-white min-h-12 flex items-center justify-between gap-6">
+        <div class="hidden md:block"></div>
+        <div class="text-sm">
+            <span class="inline-block mr-3">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</span>
+            <span class="font-medium underline">ShopNow</span>
+        </div>
+        <button>test</button>
     </div>
 </template>
 
