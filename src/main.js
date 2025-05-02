dec013b3b2e7a149e0b08b91140c84dd78ae08b3
shainadeguzman <shainajanedg@gmail.com>
2025-05-02 16:30:04 +0800

    install pinia for state management

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import './assets/main.css'
 import '@fontsource/poppins';
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import router from '@/router'
 import PhosphorIcons from "@phosphor-icons/vue"
 
+const pinia = createPinia()
 const app = createApp(App)
+
 app.use(router)
 app.use(PhosphorIcons)
+app.use(pinia)
 app.mount('#app')
