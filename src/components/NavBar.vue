e84f6afb51981ca67ce4b243b7dd033e82115b6e
shainadeguzman <shainajanedg@gmail.com>
2025-05-02 16:09:35 +0800

    add navlinks component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,11 +6,7 @@
             <RouterLink to="/">
                 <span class="text-2xl font-bold">NexoShop</span>
             </RouterLink>
-            <ul class="items-center gap-8 xl:gap-10 hidden lg:flex">
-                <li v-for="(link, index) in navLinks" :key="index">
-                    <RouterLink :to="link.to" activeClass="border-b-[1px] border-black"><span>{{ link.label }}</span></RouterLink>
-                </li>
-            </ul>
+            <NavLinks class="items-center gap-8 xl:gap-10 hidden lg:flex" />
             <BaseButtonIcon class="flex lg:hidden justify-end">
                 <template v-slot:icon>
                     <PhList :size="32" />
@@ -29,21 +25,25 @@
                 <RouterLink to="/wishlist"><PhHeart :size="26" /></RouterLink>
                 <RouterLink to="/cart"> <PhShoppingCart :size="26" /></RouterLink>
             </div>
+            <!-- Mobile Nav -->
+            <Teleport to="body">
+                <div class="fixed top-0 right-0 w-[100vw] bg-white h-screen px-4 shadow-md">
+                    <BaseButtonIcon class="flex justify-end ml-auto mt-4">
+                        <template v-slot:icon>
+                           <PhX :size="22" />
+                        </template>
+                    </BaseButtonIcon>
+                    <NavLinks class="flex flex-col gap-y-10 h-screen items-center mt-8" />
+                </div>
+            </Teleport>
         </nav>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import BaseSearchInput from '@/components/BaseSearchInput.vue'
 import BaseButtonIcon from '@/components/BaseButtonIcon.vue'
-
-const navLinks = ref([
-    { label: 'Home', to: '/' },
-    { label: 'Contact', to: '/contact' },
-    { label: 'About', to: '/about' },
-    { label: 'Log In', to: '/login' }
-])
+import NavLinks from '@/components/NavLinks.vue'
 
 const searchPlaceholder = "What are you looking for?"
 </script>
---
+++ src/components/NavLinks.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+        <li v-for="(link, index) in navLinks" :key="index">
+            <RouterLink :to="link.to" activeClass="border-b-[1px] border-black"><span>{{ link.label }}</span></RouterLink>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const navLinks = ref([
+    { label: 'Home', to: '/' },
+    { label: 'Contact', to: '/contact' },
+    { label: 'About', to: '/about' },
+    { label: 'Log In', to: '/login' }
+])
+</script>
+
+<style scoped></style>
