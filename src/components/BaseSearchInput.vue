98a9af1adc86e4529b969dc37c54a9689ad23bf7
shainadeguzman <shainajanedg@gmail.com>
2025-05-03 01:50:02 +0800

    fix

--- src/components/BaseSearchInput.vue
+++ src/components/BaseSearchInput.vue
@@ -1,16 +1,11 @@
 <template>
     <input 
         :placeholder="searchPlaceholder"
-        v-model="search"
         class="outline-none :focus-visible:outline-none focus-within:outline-none w-full lg:w-max text-sm lg:text-base"
     >
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-const search = ref('')
-
 defineProps({
     searchPlaceholder: String
 })
