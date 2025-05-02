5d006bd42363568c8e44d042fb971b8c92ea1311
shainadeguzman <shainajanedg@gmail.com>
2025-05-02 14:35:52 +0800

    update navbar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,33 +1,35 @@
 <template>
-    <nav class="px-6 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl mx-auto flex">
-        <div class="logo">NexoShop</div>
-        <ul>
-            <li>
-                <span>Home</span>
-                <span>Contact</span>
-                <span>About</span>
-                <span>Sign Up</span>
-            </li>
-        </ul>
-        <div class="py-2 px-3 bg-gray-200 flex gap-4 item-center max-w-[243px]">
-            <BaseSearchInput :searchPlaceholder="searchPlaceholder" />
-            <BaseButtonIcon>
-                <template v-slot:icon>
-                    <PhMagnifyingGlass :size="26" />
-                </template>
-            </BaseButtonIcon>
-        </div>
-        <BaseButtonIcon>
-            <template v-slot:icon>
-                <PhHeart :size="26" />
-            </template>
-        </BaseButtonIcon>
-        <BaseButtonIcon>
-            <template v-slot:icon>
-                <PhShoppingCart :size="26" />
-            </template>
-        </BaseButtonIcon>
-    </nav>
+    <div class="border-b-black border-b-[0.5px]">
+        <nav class="px-6 md:px-8 lg:px-16 xl:px-34 mx-auto w-full max-w-7xl flex items-center justify-between pt-10 pb-4">
+            <span class="text-2xl font-bold">NexoShop</span>
+            <ul class="flex items-center gap-12">
+                <li><span>Home</span></li>
+                <li><span>Contact</span></li>
+                <li><span>About</span></li>
+                <li><span>Sign Up</span></li>
+            </ul>
+            <div class="flex items-center gap-6">
+                <div class="py-2 px-4 bg-gray-200 flex gap-4 item-center rounded-sm">
+                    <BaseSearchInput :searchPlaceholder="searchPlaceholder" />
+                    <BaseButtonIcon>
+                        <template v-slot:icon>
+                            <PhMagnifyingGlass :size="22" />
+                        </template>
+                    </BaseButtonIcon>
+                </div>
+                <BaseButtonIcon>
+                    <template v-slot:icon>
+                        <PhHeart :size="26" />
+                    </template>
+                </BaseButtonIcon>
+                <BaseButtonIcon>
+                    <template v-slot:icon>
+                        <PhShoppingCart :size="26" />
+                    </template>
+                </BaseButtonIcon>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script setup>
