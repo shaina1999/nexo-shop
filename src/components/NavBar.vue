d05eb6bfa0be283802c5630c17279f7153fcbe3b
shainadeguzman <shainajanedg@gmail.com>
2025-05-02 09:48:53 +0800

    add navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <div class="logo">NexoShop</div>
+        <ul>
+            <li>
+                <span>Home</span>
+                <span>Contact</span>
+                <span>About</span>
+                <span>Sign Up</span>
+            </li>
+        </ul>
+        <div>
+            <!-- BaseSearchInput Here -->
+            <!-- BaseButtonIcon Here -->
+            <!-- BaseButtonIcon Here -->
+        </div>
+    </nav>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
