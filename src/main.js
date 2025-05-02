51e1d326af6b5e0f6e7642084ae7904069d1c961
shainadeguzman <shainajanedg@gmail.com>
2025-05-02 09:53:06 +0800

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import App from './App.vue'
 
 import router from '@/router'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(far, fas, fab);
+
 const app = createApp(App)
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app')
