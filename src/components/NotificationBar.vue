708fc7186f060ec8c19f24a7c542035be29cf6c1
shainadeguzman <shainajanedg@gmail.com>
2025-05-02 13:03:53 +0800

    update notification bar component

--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -6,26 +6,65 @@
             <span class="font-medium underline">ShopNow</span>
         </div>
         <div class="relative">
-            <button class="text-sm flex item-center gap-1 sm:gap-2 cursor-pointer">
-                <span class="inline-block">English</span>
+            <button 
+                class="text-sm flex item-center gap-1 sm:gap-2 cursor-pointer"
+                @click="showDropdown = !showDropdown"
+                ref="dropdown-button"
+            >
+                <span class="inline-block">{{ selectedLanguageStr }}</span>
                 <PhCaretDown :size="20" />
             </button>
-            <ul class="text-sm bg-black absolute py-3 px-4 flex flex-col gap-y-3 rounded-sm text-white top-10 right-0">
-                <li class="relative flex items-center gap-2 hover:underline">
-                    <span>Filipino</span>
-                    <PhCheck :size="20" class="hidden" />
-                    <input type="radio" name="language" class="opacity-0 absolute top-0 left-0 right-0 bottom-0 cursor-pointer">
-                </li>
-                <li class="relative flex items-center gap-2 hover:underline font-bold">
-                    <span>English</span>
-                    <PhCheck :size="20" />
-                    <input type="radio" name="language" class="opacity-0 absolute top-0 left-0 right-0 bottom-0 cursor-pointer">
+            <ul 
+                class="text-sm bg-black absolute py-3 px-4 flex flex-col gap-y-3 rounded-sm text-white top-10 right-0"
+                :class="{ 'hidden': !showDropdown, 'block' : showDropdown }"
+            >
+                <li 
+                    class="relative flex items-center gap-2 hover:underline"
+                    :class="{ 'font-bold' : languageOption.active }"
+                    v-for="(languageOption, index) in languageOptions" :key="index"
+                >
+                    <span>{{ languageOption.label }}</span>
+                    <PhCheck :size="20" :class="{ 'hidden' : !languageOption.active }" />
+                    <input 
+                        type="radio"
+                        :value="languageOption.value"
+                        @change="updateLanguage(languageOption.value)" 
+                        name="language" 
+                        class="opacity-0 absolute top-0 left-0 right-0 bottom-0 cursor-pointer"
+                    >
                 </li>
             </ul>
         </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed, ref, watch, useTemplateRef} from 'vue'
+import { onClickOutside } from '@vueuse/core'
+
+const languageOptions = ref([
+    {id: 1, value: 'english', label: 'English', active: true},
+    {id: 2, value: 'filipino', label: 'Filipino', active: false}
+])
+
+const selectedLanguage = ref(languageOptions.value.find(opt => opt.active)?.value || null)
+const selectedLanguageStr = ref("English")
+const dropdownButtonRef = useTemplateRef('dropdown-button')
+const showDropdown = ref(false)
+
+const updateLanguage = (newLanguage) => {
+    selectedLanguage.value = newLanguage
+
+    languageOptions.value.forEach(option => {
+        option.active = option.value === newLanguage
+
+        if (option.active) {
+            selectedLanguageStr.value = option.label
+        }
+    })
+}
+
+onClickOutside(dropdownButtonRef, event => showDropdown.value = false)
+</script>
 
 <style scoped></style>
