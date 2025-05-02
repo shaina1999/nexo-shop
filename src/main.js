171b667972d722c5f0ebcb087c37fa3610ee1ed2
shainadeguzman <shainajanedg@gmail.com>
2025-05-02 10:37:47 +0800

    replace fontawesome with PhosphorIcons

--- src/main.js
+++ src/main.js
@@ -5,15 +5,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from '@/router'
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-library.add(far, fas, fab);
+import PhosphorIcons from "@phosphor-icons/vue"
 
 const app = createApp(App)
 app.use(router)
-app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(PhosphorIcons)
 app.mount('#app')
