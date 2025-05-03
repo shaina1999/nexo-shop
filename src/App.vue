3003abb8ba10ecb99ff94b1b9e2e0de464cf5993
shainadeguzman <shainajanedg@gmail.com>
2025-05-04 04:09:24 +0800

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup>
+import { useElementSize } from '@vueuse/core'
+import { computed, useTemplateRef, ref } from 'vue'
+
 import NotificationBar from '@/components/NotificationBar.vue'
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+
+const notificationBarRef = useTemplateRef('notificationBarRef')
+const { width, height } = useElementSize(notificationBarRef)
+const windowWidth = ref(window.innerWidth)
+const headerHeight = computed(() => windowWidth.value > 1024 ? 97 : 118)
 </script>
 
 <template>
     <div class="fixed top-0 left-0 right-0">
-        <NotificationBar class="notificationBarRef" />
-        <Header class="headerRef" />
+        <NotificationBar ref="notificationBarRef" />
+        <Header />
     </div>
-    <div><RouterView /></div>
+    <div :style="{ marginTop: `${height + headerHeight}px` }">
+        <RouterView />
+    </div>
     <Footer />
 </template>
