69b82a521a6dc295d000911168e3fad4369402ac
shainadeguzman <shainajanedg@gmail.com>
2025-05-03 10:35:20 +0800

    update fetch.js

--- src/composables/fetch.js
+++ src/composables/fetch.js
@@ -1,22 +1,34 @@
 import { ref, watchEffect, toValue } from 'vue'
+import { API_BASE_URL } from '@/config';
 
 export function useFetch(url) {
   const data = ref(null)
   const error = ref(null)
+  const isLoading = ref(false)
 
   const fetchData = () => {
+    const fullUrl = API_BASE_URL + toValue(url)
+    
     data.value = null
     error.value = null
-
-    fetch(toValue(url))
+    isLoading.value = true
+    
+    fetch(fullUrl)
       .then((res) => res.json())
-      .then((json) => (data.value = json))
-      .catch((err) => (error.value = err))
+      .then((json) => {
+        data.value = json
+      })
+      .catch((err) => {
+        error.value = err
+      })
+      .finally(() => {
+        isLoading.value = false
+      })
   }
 
   watchEffect(() => {
     fetchData()
   })
 
-  return { data, error }
+  return { data, error, isLoading }
 }
