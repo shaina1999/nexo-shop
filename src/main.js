ebf078522927988f2d344bd92b21440e8114fba7
shainadeguzman <shainajanedg@gmail.com>
2025-05-05 15:00:14 +0800

    install vue-i18n version 11

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '@fontsource/poppins';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
 import router from '@/router'
@@ -11,6 +12,11 @@ import PhosphorIcons from "@phosphor-icons/vue"
 const pinia = createPinia()
 const app = createApp(App)
 
+const i18n = createI18n({
+  // something vue-i18n options here ...
+})
+
+app.use(i18n)
 app.use(router)
 app.use(PhosphorIcons)
 app.use(pinia)
