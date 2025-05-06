6d23bba65770f259746b583d6cb09975cddc8cb7
shainadeguzman <shainajanedg@gmail.com>
2025-05-06 10:22:47 +0800

    update login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="w-full flex items-center justify-center pt-15 relative h-[781px]">
         <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl grid grid-cols-2 gap-32">
-            <div class="absolute left-0 top-15 bottom-0 right-[50%]">
+            <div class="absolute left-0 top-15 bottom-0 right-[50%] hidden lg:flex">
                 <img :src="authRightImage" alt="Ecommerce illustration" class="w-full h-full object-cover login-image">
             </div>
-            <div class="h-[706px]"></div>
+            <div class="h-[706px] hidden lg:flex"></div>
             <div class="w-full flex justify-center flex-col">
                 <div>Log in to Exclusive</div>
                 <div>Enter your details below</div>
