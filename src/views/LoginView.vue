eacd916ec4877b1eae0d1806290d2731a240667a
shainadeguzman <shainajanedg@gmail.com>
2025-05-06 08:01:04 +0800

    update login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,26 @@
 <template>
+    <div class="pt-15 flex gap-20 pr-4 md:pr-8 lg:pr-16 xl:pr-34 w-full max-w-7xl">
+        <div>
+            <img :src="authRightImage" alt="Ecommerce illustration" class="w-full shrink">
+        </div>
+        <div>
+            <div>Log in to Exclusive</div>
+            <div>Enter your details below</div>
+
+            <form>
+                <input type="text" placeholder="Email or Phone Number">
+                <input type="password" placeholder="Password">
+                <div>
+                    <button>Submit</button>
+                    <div>Forgot Password?</div>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import authRightImage from '@/assets/img/login-signup-image.png'
+</script>
 
 <style scoped></style>
