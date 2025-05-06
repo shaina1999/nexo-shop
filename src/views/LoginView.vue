5123a4d8a997c32c61914e0f55d5804767698e60
shainadeguzman <shainajanedg@gmail.com>
2025-05-06 11:02:54 +0800

    fix

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full flex items-center justify-center pt-15 relative h-[781px]">
         <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl grid grid-cols-2 gap-32">
-            <div class="absolute left-0 top-15 bottom-0 right-[50%] hidden lg:flex">
+            <div class="absolute left-0 top-15 bottom-0 right-[52%] hidden lg:flex">
                 <img :src="authRightImage" alt="Ecommerce illustration" class="w-full h-full object-cover login-image">
             </div>
             <div class="h-[706px] hidden lg:flex"></div>
