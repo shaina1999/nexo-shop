2e0538dde6043656309c348b6743c19f4aea0d6e
shainadeguzman <shainajanedg@gmail.com>
2025-05-06 19:52:09 +0800

    update sign up view

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import 'nprogress/nprogress.css'
 
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import SignUpView from '@/views/SignUpView.vue'
 import ForgotPasswordView from '@/views/ForgotPasswordView.vue'
 import SearchView from '@/views/SearchView.vue'
 import NotFoundComponent from '@/components/NotFoundComponent.vue'
@@ -13,6 +14,7 @@ const routes = [
   { path: '/home', name:'home', component: HomeView },
   { path: '/search', name:'search', component: SearchView },
   { path: '/login', name:'login', component: LoginView },
+  { path: '/sign-up', name:'sign-up', component: SignUpView },
   { path: '/forgot-password', name:'forgot-password', component: ForgotPasswordView },
   { path: '/:pathMatch(.*)', component: NotFoundComponent }
 ]
