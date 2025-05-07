c560861461cf467fb1df7a4227b1cf97e5b3ef5b
shainadeguzman <shainajanedg@gmail.com>
2025-05-07 09:17:45 +0800

    update footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,48 @@
 <template>
+    <div class="mt-20 bg-black">
+        <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl py-20 text-white">
+            <div class="text-2xl font-bold">NexoShop</div>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+                <div>
+                    <div>Subscribe</div>
+                    <ul>
+                        <li>Get 10% off your first order</li>
+                        <li>
+                            <input type="text">
+                            <button></button>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <div>Support</div>
+                    <ul>
+                        <li>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</li>
+                        <li>nexoshop@gmail.com</li>
+                        <li>+88015-88888-9999</li>
+                    </ul>
+                </div>
+                <div>
+                    <div>Account</div>
+                    <ul>
+                        <li>My Account</li>
+                        <li>Login / Register</li>
+                        <li>Cart</li>
+                        <li>Wishlist</li>
+                        <li>Shop</li>
+                    </ul>
+                </div>
+                <div>
+                    <div>Quick Link</div>
+                    <ul>
+                        <li>Privacy Policy</li>
+                        <li>Terms Of Use</li>
+                        <li>FAQ</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup></script>
