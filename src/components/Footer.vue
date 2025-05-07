fdefc8106994a418672d8297942aac9fe2969018
shainadeguzman <shainajanedg@gmail.com>
2025-05-07 10:57:29 +0800

    update footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,28 @@
 <template>
-    <div class="mt-20 bg-black">
-        <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl py-20 text-white">
-            <div class="text-2xl font-bold">NexoShop</div>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+    <div class="mt-20 bg-black w-full flex items-center justify-center">
+        <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl pt-20 text-white">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-20">
                 <div>
-                    <div>Subscribe</div>
-                    <ul>
+                    <div class="text-lg font-bold mb-6">NexoShop</div>
+                    <ul class="flex flex-col gap-y-4">
                         <li>Get 10% off your first order</li>
-                        <li>
-                            <input type="text">
-                            <button></button>
+                        <li class="border-[1.5px] border-white rounded-sm flex gap-1 p-2">
+                            <input type="text" class="outline-none text-white w-full shrink" placeholder="Enter your email">
+                            <button class="cursor-pointer"><PhPaperPlaneRight :size="22" /></button>
                         </li>
                     </ul>
                 </div>
                 <div>
-                    <div>Support</div>
-                    <ul>
+                    <div class="text-lg font-semibold mb-6">Support</div>
+                    <ul class="flex flex-col gap-y-4">
                         <li>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</li>
                         <li>nexoshop@gmail.com</li>
                         <li>+88015-88888-9999</li>
                     </ul>
                 </div>
                 <div>
-                    <div>Account</div>
-                    <ul>
+                    <div class="text-l font-semibold mb-6">Account</div>
+                    <ul class="flex flex-col gap-y-4">
                         <li>My Account</li>
                         <li>Login / Register</li>
                         <li>Cart</li>
@@ -32,8 +31,8 @@
                     </ul>
                 </div>
                 <div>
-                    <div>Quick Link</div>
-                    <ul>
+                    <div class="text-lg font-semibold mb-6">Quick Link</div>
+                    <ul class="flex flex-col gap-y-4">
                         <li>Privacy Policy</li>
                         <li>Terms Of Use</li>
                         <li>FAQ</li>
@@ -42,6 +41,9 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="bg-black pt-20">
+        <p class="px-4 md:px-8 lg:px-16 xl:px-34 text-sm md:text-base text-center text-white py-5 border-t-[1px] border-t-white opacity-[40%]">&copy; Copyright NexoShop 2025. All right reserved</p>
     </div>
 </template>
 
