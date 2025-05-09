d26e30a35ea1a1e93ae5231fc201c4c3d5244726
shainadeguzman <shainajanedg@gmail.com>
2025-05-09 10:48:22 +0800

    update product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
-    <button @click="router.push({ path: '/product', query: { id: product.id} } ); " class="cursor-pointer">
+    <article @click="goToProductPage(product.id)" class="cursor-pointer">
         <div class="bg-gray-200 p-4 product cursor-pointer">
-            <div class="flex items-center justify-between mb-4">
+            <header class="flex items-center justify-between mb-4">
                 <div class="bg-secondary-500 text-white py-1 px-2 rounded-sm text-sm">{{ product.discount }}%</div>
                 <button 
                     class="cursor-pointer bg-white flex items-center justify-center text-secondary-500 w-10 h-10 rounded-full transition-all duration-300 ease-in-out"
@@ -10,40 +10,42 @@
                     <PhHeart :size="24" 
                 />
                 </button>
-            </div>
-            <div class="w-[190px] h-[190px] flex items-center justify-center mx-auto">
+            </header>
+            <figure class="w-[190px] h-[190px] flex items-center justify-center mx-auto">
                 <img :src="product.image" :alt="product.name" class="w-full">
-            </div>
+            </figure>
         </div>
-        <button 
-            class="bg-black text-white w-full p-1.5 transition-all duration-300 ease-in-out cursor-pointer add-to-cart mb-4 flex items-center justify-center gap-x-2"
-            @click.prevent.stop="addToCart"
-        >
-            <span class="text-sm">Add To Cart</span>
-            <PhPlus :size="18" />
-            <PhCheck :size="18" class="hidden" />
-        </button>
-        <div>
-            <div class="font-semibold text-base mb-2.5">{{ product.name }}</div>
-            <div class="flex items-center gap-x-2.5 mb-2.5">
-                <span class="text-secondary-500">&#8369;{{ product.discountedPrice }}</span>
-                <del class="text-gray-500 decoration-dashed">&#8369;{{ product.price }}</del>
-            </div>
+        <footer>
             <button 
-                class="flex items-center gap-x-2 cursor-pointer"
-                @click.prevent.stop="viewProductReviews"
+                class="bg-black text-white w-full p-1.5 transition-all duration-300 ease-in-out cursor-pointer add-to-cart mb-4 flex items-center justify-center gap-x-2"
+                @click.prevent.stop="addToCart"
             >
-                <div class="flex items-center text-orange-500 gap-x-1">
-                    <PhStar :size="19" weight="fill" />
-                    <PhStar :size="19" weight="fill" />
-                    <PhStar :size="19" weight="fill" />
-                    <PhStarHalf :size="19" weight="fill" />
-                    <PhStar :size="19" />
-                </div>
-                <span class="text-gray-500">{{ `(${product.reviewsCount})` }}</span>
+                <span class="text-sm">Add To Cart</span>
+                <PhPlus :size="18" />
+                <PhCheck :size="18" class="hidden" />
             </button>
-        </div>
-    </button>
+            <div>
+                <h2 class="font-semibold text-base mb-2.5">{{ product.name }}</h2>
+                <div class="flex items-center gap-x-2.5 mb-2.5">
+                    <span class="text-secondary-500">&#8369;{{ product.discountedPrice }}</span>
+                    <del class="text-gray-500 decoration-dashed">&#8369;{{ product.price }}</del>
+                </div>
+                <button 
+                    class="flex items-center gap-x-2 cursor-pointer"
+                    @click.prevent.stop="viewProductReviews"
+                >
+                    <div class="flex items-center text-orange-500 gap-x-1">
+                        <PhStar :size="19" weight="fill" />
+                        <PhStar :size="19" weight="fill" />
+                        <PhStar :size="19" weight="fill" />
+                        <PhStarHalf :size="19" weight="fill" />
+                        <PhStar :size="19" />
+                    </div>
+                    <span class="text-gray-500">{{ `(${product.reviewsCount})` }}</span>
+                </button>
+            </div>
+        </footer>
+    </article>
 </template>
 
 <script setup>
@@ -54,6 +56,10 @@ const router = useRouter()
 defineProps({
     product: Object
 })
+
+const goToProductPage = (id) => {
+    router.push({ path: '/product', query: { id: id} } ); 
+}
 
 const addToWishList = () => {
     console.log('add to wish list')
