6899aa854388002689e529da2f926e0a9d128874
shainadeguzman <shainajanedg@gmail.com>
2025-05-16 14:08:59 +0800

    fix

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -54,6 +54,7 @@
                   name="category" 
                   :value="category.label" 
                   type="checkbox" 
+                  v-model="category.isSelected"
                   class="absolute left-0 right-0 top-0 bottom-0 w-full h-full opacity-0 cursor-pointer" 
                   @change="(event) => handleCategoryChange(event, index)"
                 >
