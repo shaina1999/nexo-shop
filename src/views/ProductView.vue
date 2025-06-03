ab2688778d91e39bf5fd8c0e772937e7c8a0d890
shainadeguzman <shainajanedg@gmail.com>
2025-06-03 18:10:05 +0800

    add product details

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -107,7 +107,12 @@
               <span class="text-xs sm:text-sm font-semibold bg-secondary-100 text-secondary-500 rounded-sm px-1">-{{ productObj?.discount }}%</span>
             </p>
           </header>
-          <p class="text-gray-600 lead-[1.7]">{{ productObj?.description }}</p>
+          <p class="text-gray-600 lead-[1.7] mb-2">{{ productObj?.description }}</p>
+          <div v-if="productObj?.details" class="space-y-1">
+            <div v-for="(value, key) in productObj.details" :key="key" class="text-sm sm:text-base">
+              <strong class="uppercase font-bold">{{ key }}:</strong> <span class="text-gray-600">{{ value }}</span>
+            </div>
+          </div>
           <!-- Quantity and Action Buttons -->
           <section class="flex flex-col gap-8">
             <div class="flex flex-col">
