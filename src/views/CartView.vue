a0e6953ea6d61b4b9a76768d60618ee37a39131d
shainadeguzman <shainajanedg@gmail.com>
2025-06-05 07:50:08 +0800

    update cart buttons

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -2,12 +2,12 @@
     <section class="flex items-center justify-center w-full pt-5 md:pt-10 product-details">
         <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl pb-16 sm:pb-20 md:pb-25">
             <h2 class="inline-block md:flex items-center gap-x-3 text sm sm:text-base md:text-lg pb-5 md:pb-10">
-                <span class="font-semibold">My Cart</span><span class="font-regular text-gray-500">(2 items)</span>
+                <span class="font-semibold">My Cart 🛒</span><span class="font-regular text-gray-500">(2 items)</span>
             </h2>
             <!-- Cart Table -->
-            <div class="overflow-x-auto">
-                <table class="min-w-full bg-white text-left border border-gray-200 rounded-lg">
-                    <thead class="bg-gray-100 text-gray-600 text-sm">
+            <div class="overflow-x-auto pb-1">
+                <table class="min-w-full bg-white text-left rounded-lg">
+                    <thead class="text-black text-sm shadow">
                         <tr>
                             <th class="px-4 py-3">
                                 <input type="checkbox" />
@@ -16,11 +16,11 @@
                             <th class="px-4 py-3">Price</th>
                             <th class="px-4 py-3">Quantity</th>
                             <th class="px-4 py-3">Subtotal</th>
-                            <th class="px-4 py-3">Action</th>
+                            <th class="px-4 py-3"></th>
                         </tr>
                     </thead>
                     <tbody class="text-gray-700 text-sm">
-                        <tr class="border-t">
+                        <tr class="shadow">
                             <td class="px-4 py-4">
                                 <input type="checkbox" />
                             </td>
@@ -41,7 +41,7 @@
                                 <button class="text-red-500 hover:underline">Remove</button>
                             </td>
                         </tr>
-                        <tr class="border-t">
+                        <tr class="!shadow">
                             <td class="px-4 py-4">
                                 <input type="checkbox" />
                             </td>
@@ -67,16 +67,22 @@
             </div>
 
             <!-- Actions -->
-            <div class="flex flex-col md:flex-row justify-between mt-6 gap-4">
+            <div class="flex items-start flex-col md:flex-row justify-between mt-6 gap-4">
                 <div class="flex flex-col sm:flex-row gap-3 w-full md:w-auto">
-                    <button
-                        class="px-6 py-3 border border-gray-300 text-gray-700 rounded hover:bg-gray-100 w-full sm:w-auto">
-                        Return to Shop
-                    </button>
-                    <button
-                        class="px-6 py-3 border border-red-500 text-red-600 rounded hover:bg-red-50 w-full sm:w-auto">
-                        Remove Selected Items
-                    </button>
+                    <BaseLinkButton 
+                        to="/products" 
+                        class="!flex items-center gap-x-1.5 text-sm !px-4.5 !py-2.5 md:!px-6 md:!py-3 md:mx-0 text-center !w-full md:!w-auto bg-white !text-black border-[1px] border-black/50 hover:!bg-gray-200"
+                    >
+                        Back to Shop
+                        <PhArrowUDownLeft :size="18" />
+                    </BaseLinkButton>
+                    <BaseButton 
+                        :disabled="disableRemoveButton"
+                        class="!flex items-center justify-center gap-x-1.5 text-sm !px-4.5 !py-2.5 md:!px-6 md:!py-3 mx-auto md:mx-0 w-full md:w-auto !bg-red-500 !text-white border-[1px] border-red/50 hover:!bg-red-600"
+                    >
+                        Remove
+                        <PhTrash :size="18" />
+                    </BaseButton>
                 </div>
 
                 <!-- Cart Total -->
@@ -104,4 +110,9 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import BaseLinkButton from '@/components/BaseLinkButton.vue'
+import BaseButton from '@/components/BaseButton.vue'
+
+const disableRemoveButton = ref(true)
 </script>
