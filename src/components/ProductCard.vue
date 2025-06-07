803b10ddb6dc74907e961c8a62dd28c043f60c30
shainadeguzman <shainajanedg@gmail.com>
2025-06-07 13:46:32 +0800

    add toast and error handling to addToCart function

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -56,6 +56,7 @@ import { ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useCurrencyFormat } from '@/composables/currencyFormat'
 import { useCartStore } from '@/stores/cartStore'
+import Swal from 'sweetalert2'
 
 const router = useRouter()
 const { formatAmount } = useCurrencyFormat()
@@ -76,8 +77,20 @@ const addToWishList = () => {
 
 const addToCart = async () => {
     isAddingToCart.value = true
-    const cartItem = { product_id: props.product.id, quantity: 1, }
-    await cart.addToCart(cartItem)
-    isAddingToCart.value = false
+
+    try {
+        const cartItem = { product_id: props.product.id, quantity: 1 }
+        await cart.addToCart(cartItem)
+    } catch (err) {
+        console.error('Failed to add item to cart:', err)
+         Swal.fire({
+            icon: 'error',
+            title: 'Oops!',
+            text: 'Failed to add item to your bag. Please try again.',
+            confirmButtonText: 'OK'
+        })
+    } finally {
+        isAddingToCart.value = false
+    }
 }
 </script>
