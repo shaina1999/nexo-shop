3e69734549b949c592d3ad099f4464205c5e29fc
shainadeguzman <shainajanedg@gmail.com>
2025-06-09 18:03:25 +0800

    update checkout page

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,59 +1,65 @@
 <template>
     <section class="flex items-center justify-center w-full pt-5 md:pt-10">
         <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl pb-16 sm:pb-20 md:pb-25">
-           <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+           <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-8">
                 <!-- Billing Details -->
                 <div>
-                    <h2 class="inline-block md:flex items-center gap-x-3 text sm sm:text-base md:text-lg pb-5 lg:pb-10">Billing Details</h2>
-                    <form class="space-y-4">
-                    <div>
-                        <label class="block text-sm font-medium">Full Name<span class="text-red-500">*</span></label>
-                        <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
-                    </div>
-                    <div>
-                        <label class="block text-sm font-medium">Street Address<span class="text-red-500">*</span></label>
-                        <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
-                    </div>
-                    <div>
-                        <label class="block text-sm font-medium">Apartment, floor, etc. <span class="text-gray-500 text-xs">(Optional)</span></label>
-                        <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
-                    </div>
-                    <div>
-                        <label class="block text-sm font-medium">Town/City<span class="text-red-500">*</span></label>
-                        <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
-                    </div>
-                    <div>
-                        <label class="block text-sm font-medium">Phone Number<span class="text-red-500">*</span></label>
-                        <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
-                    </div>
-                    <div>
-                        <label class="block text-sm font-medium">Email Address <span class="text-gray-500 text-xs">(Optional)</span></label>
-                        <input type="email" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
-                    </div>
-                    <div class="flex items-center">
-                         <label class="relative cursor-pointer flex items-center gap-1.5">
-                            <input 
-                                type="checkbox" 
-                                class="absolute left-0 right-0 top-0 bottom-0 w-full h-full opacity-0 cursor-pointer" 
-                            >
-                            <span 
-                                class="w-4.5 h-4.5 rounded-sm border-[1px] border-black flex items-center justify-center text-white relative"
-                            >
-                                <PhCheckFat 
-                                    :size="12" 
-                                    weight="fill" 
-                                    class="absolute top-[50%] -translate-x-1/2 left-1/2 -translate-y-1/2 text-transparent"
-                                />
-                            </span>
-                            <span class="text-sm">Save this information for faster check-out next time </span>
-                        </label>
-                    </div>
+                    <h2 class="inline-block md:flex items-center gap-x-3 text sm sm:text-base md:text-lg pb-2.5 lg:pb-6">Billing Details</h2>
+                    <button class="border border-gray-300 p-4 sm:p-6 rounded-lg shadow w-full text-left flex flex-col items-start text-sm sm:text-base">
+                        <span class="inline-block mb-0.5">Shaina De Guzman</span>
+                        <span class="inline-block mb-0.5">09703498334</span>
+                        <span class="inline-block mb-0.5">shaina@gmail.com</span>
+                        <span class="inline-block mb-0.5">182 J.P Rizal Street, Barangay 2nd District Jalajala, Rizal</span>
+                    </button>
+                    <form class="space-y-4 hidden">
+                        <div>
+                            <label class="block text-sm font-medium">Full Name<span class="text-red-500">*</span></label>
+                            <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
+                        </div>
+                        <div>
+                            <label class="block text-sm font-medium">Street Address<span class="text-red-500">*</span></label>
+                            <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
+                        </div>
+                        <div>
+                            <label class="block text-sm font-medium">Apartment, floor, etc. <span class="text-gray-500 text-xs">(Optional)</span></label>
+                            <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
+                        </div>
+                        <div>
+                            <label class="block text-sm font-medium">Town/City<span class="text-red-500">*</span></label>
+                            <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
+                        </div>
+                        <div>
+                            <label class="block text-sm font-medium">Phone Number<span class="text-red-500">*</span></label>
+                            <input type="text" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
+                        </div>
+                        <div>
+                            <label class="block text-sm font-medium">Email Address <span class="text-gray-500 text-xs">(Optional)</span></label>
+                            <input type="email" class="mt-1 w-full border border-gray-300 rounded-md p-2 focus-visible:!outline-none focus-visible:!border-secondary-500 transition-colors duration-300 ease-in-out" />
+                        </div>
+                        <div class="flex items-center">
+                            <label class="relative cursor-pointer flex items-center gap-1.5">
+                                <input 
+                                    type="checkbox" 
+                                    class="absolute left-0 right-0 top-0 bottom-0 w-full h-full opacity-0 cursor-pointer" 
+                                >
+                                <span 
+                                    class="w-4.5 h-4.5 rounded-sm border-[1px] border-black flex items-center justify-center text-white relative"
+                                >
+                                    <PhCheckFat 
+                                        :size="12" 
+                                        weight="fill" 
+                                        class="absolute top-[50%] -translate-x-1/2 left-1/2 -translate-y-1/2 text-transparent"
+                                    />
+                                </span>
+                                <span class="text-sm">Save this information for faster check-out next time </span>
+                            </label>
+                        </div>
                     </form>
                 </div>
 
                 <!-- Order Summary -->
-                <div class="space-y-6">
-                    <h2 class="inline-block md:flex items-center gap-x-3 text sm sm:text-base md:text-lg pb-5 lg:pb-10 mb-0">Order Summary</h2>
+                <div class="space-y-5">
+                    <h2 class="inline-block md:flex items-center gap-x-3 text sm sm:text-base md:text-lg pb-2.5 lg:pb-6 mb-0">Order Summary</h2>
                     <div class="border border-gray-300 p-6 rounded-lg space-y-8 lg:space-y-6 shadow">
                         <div class="flex items-baseline md:items-center lg:items-center justify-between gap-2 lg:gap-12 flex-col md:flex-row">
                             <div class="flex items-center gap-3 grow-0">
