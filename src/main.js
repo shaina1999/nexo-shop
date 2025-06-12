31996dd1526a6836c8d41d46a595f10638938805
shainadeguzman <shainajanedg@gmail.com>
2025-06-12 17:47:01 +0800

    add v-select component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/css/main.css'
 import '@fontsource/poppins'
 import '@splidejs/vue-splide/css'
+import 'vue-select/dist/vue-select.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -11,6 +12,7 @@ import PhosphorIcons from '@phosphor-icons/vue'
 import VueCountdown from '@chenfengyuan/vue-countdown'
 import VueSplide from '@splidejs/vue-splide'
 import VueEasyLightbox from 'vue-easy-lightbox'
+import vSelect from 'vue-select'
 
 const pinia = createPinia()
 const app = createApp(App)
@@ -20,5 +22,6 @@ app.use(PhosphorIcons)
 app.use(pinia)
 app.use(VueEasyLightbox)
 app.use(VueSplide)
+app.component('v-select', vSelect)
 app.component(VueCountdown.name, VueCountdown)
 app.mount('#app')
