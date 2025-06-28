019aead93dbda2e82cbee181ff6ceccc9fd90a42
shainadeguzman <shainajanedg@gmail.com>
2025-06-28 19:56:19 +0800

    update product reviews section

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -154,7 +154,7 @@
     <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl pb-16 sm:pb-20 md:pb-25">
       <div class="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4 border-t border-gray-300 pt-12 mb-6">
         <div>
-          <h3 class="text-lg sm:text-xl font-semibold text-muted-800 mb-2">Customer Reviews <span class="text-sm text-gray-500 font-normal">(100 reviews)</span></h3>
+          <h3 class="text-lg sm:text-xl font-semibold text-muted-800 mb-2">Customer Reviews <span class="text-sm text-gray-500 font-normal">{{ `(${productObj?.reviews_count} review${productObj?.reviews_count <= 1 ? '' : 's'})` }}</span></h3>
           <div class="flex items-center gap-x-2">
             <div class="flex items-center gap-1 text-yellow-400">
               <PhStar class="w-5 h-5 sm:w-6 sm:h-6" weight="fill" />
@@ -163,7 +163,7 @@
               <PhStar class="w-5 h-5 sm:w-6 sm:h-6" weight="fill" />
               <PhStar class="w-5 h-5 sm:w-6 sm:h-6" weight="fill" />
             </div>
-            <p class="text-xl sm:text-2xl text-gray-700 font-bold">4.9 out of 5</p>
+            <p class="text-xl sm:text-2xl text-gray-700 font-bold">{{ productObj?.rating }} out of 5</p>
           </div>
         </div>
         <BaseDropdown
@@ -193,13 +193,6 @@
           </p>
           <p class="text-sm text-gray-500 mt-2">Reviewed on June 01, 2024 10:01 PM</p>
         </div>
-      </div>
-
-      <!-- Load More Button -->
-      <div class="text-center">
-        <BaseButton size="sm" class="mt-6 md:mt-12">
-          Load more reviews
-        </BaseButton>
       </div>
     </div>
   </section>
@@ -231,7 +224,7 @@ const isAddingToCart = ref(false)
 const cart = useCartStore()
 const visible = ref(false)
 const selectedSort = ref('5 Star')
-const sortOptions = [ '5 Star', '4 Star' ]
+const sortOptions = [ '5 Star', '4 Star', '3 Star', '2 Star', '1 Star']
 
 const openImageViewer = () => {
   visible.value = true
