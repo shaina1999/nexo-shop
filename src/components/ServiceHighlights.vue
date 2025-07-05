c196aa1891c062569328b557f048f17fe10f2407
shainadeguzman <shainajanedg@gmail.com>
2025-07-05 19:45:37 +0800

    fix

--- src/components/ServiceHighlights.vue
+++ src/components/ServiceHighlights.vue
@@ -3,7 +3,7 @@
         <div class="px-4 md:px-8 lg:px-16 xl:px-34 w-full max-w-7xl">
             <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 sm:gap-20 items-start">
                 <li class="flex flex-col items-center justify-center">
-                    <div class="mb-mb-3 sm:6 w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center rounded-full bg-zinc-300">
+                    <div class="mb-3 sm:mb-6 w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center rounded-full bg-zinc-300">
                         <div class="w-12 h-12 sm:w-14.5 sm:h-14.5 flex items-center justify-center rounded-full bg-black text-white">
                             <PhTruck :size="32" />
                         </div>
